ae2f5fd3213ef2b0fcfe660bf0f77af901bc78cc
João Lopes <joaopalosi@gmail.com>
2024-02-12 20:48:50 -0300

    feature(portfolio): Create Project Card Component

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 
 
 @NgModule({
   declarations: [
-    PortfolioComponent
+    PortfolioComponent,
+    ProjectCardComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/portfolio/project-card/project-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.scss']
+})
+export class ProjectCardComponent {
+
+}
