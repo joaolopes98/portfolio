a1eec97dfb762a61b4669bbe29d8b5229d7503ed
João Lopes <joaopalosi@gmail.com>
2024-02-12 20:01:14 -0300

    feat(portfolio): Create Portfolio Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutModule } from './about/about.module';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     HomeComponent,
+    PortfolioComponent,
   ],
   imports: [
     BrowserModule,
