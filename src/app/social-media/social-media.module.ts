33fd40e6dbcf53ac350cbb785f272ff820f2d329
João Lopes <joaopalosi@gmail.com>
2024-02-29 19:42:02 -0300

    feat(social-media): create social-media component

---
+++ src/app/social-media/social-media.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialMediaComponent } from './social-media.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { bootstrapGithub, bootstrapInstagram, bootstrapLinkedin, bootstrapWhatsapp } from '@ng-icons/bootstrap-icons';
+
+
+
+@NgModule({
+  declarations: [
+    SocialMediaComponent
+  ],
+  imports: [
+    CommonModule,
+    NgIconsModule.withIcons({bootstrapLinkedin, bootstrapGithub, bootstrapInstagram, bootstrapWhatsapp})
+  ],
+  exports: [
+    SocialMediaComponent
+  ]
+})
+export class SocialMediaModule { }
