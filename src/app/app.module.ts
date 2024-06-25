1ad7a340207db53f94c399dc36bb14b167e49f68
João Lopes <joaopalosi@gmail.com>
2024-06-24 22:28:56 -0300

    feat(about): installing carousel for the About section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,8 @@ import { heroQueueListSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
     PortfolioModule,
     ExperienceModule,
     ContactModule,
+    RouterModule,
+    BrowserAnimationsModule,
     NgIconsModule.withIcons({ heroQueueListSolid, heroXMarkSolid }),
   ],
   providers: [],
