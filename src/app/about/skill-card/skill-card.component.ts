40f037d31de9a46309a2360585cf72a04d9b9ce0
João Lopes <joaopalosi@gmail.com>
2025-04-25 16:13:24 -0300

    feat(skill-card): automatic calculation of years of experience with a given skill

--- src/app/about/skill-card/skill-card.component.ts
+++ src/app/about/skill-card/skill-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Skill } from 'src/models/interfaces/skill';
+import { DateUtilsService } from 'src/app/shared/date-utils.service';
 
 @Component({
   selector: 'app-skill-card',
@@ -9,6 +10,14 @@ import { Skill } from 'src/models/interfaces/skill';
 })
 export class SkillCardComponent {
   @Input() skill!: Skill;
+
+  constructor(private dateUtils: DateUtilsService) {}
+
+  get time(): string {
+    if (!this.skill?.date) return '';
+    const years = this.dateUtils.calculateYearsSince(this.skill.date);
+    return `${years} years`;
+  }
 
   openLink() {
     if (this.skill.name === 'Alura') {
